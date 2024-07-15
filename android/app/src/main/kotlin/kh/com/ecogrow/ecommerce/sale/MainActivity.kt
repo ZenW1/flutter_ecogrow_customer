8cc6e5042f1fa2c2eb17c3e6e3e803987fd30c24
David Long <davitlongkhmer@gmail.com>
2024-07-15 13:31:18 +0700

    first_commit

---
+++ android/app/src/main/kotlin/kh/com/ecogrow/ecommerce/sale/MainActivity.kt
@@ -0,0 +1,6 @@
+package kh.com.ecogrow.ecommerce.sale.flutter_ecogrow_customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_ecogrow_customer/app/app.dart';
+import 'package:flutter_ecogrow_customer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
