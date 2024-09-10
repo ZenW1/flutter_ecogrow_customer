17097fc903bd0c6c20f2cad63068f0be6274db1d
David Long <davitlongkhmer@gmail.com>
2024-09-10 13:55:25 +0700

    splash screen

---
+++ lib/splash/view/splash_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecogrow_customer/authentication/authentication_bloc.dart';
+import 'package:flutter_ecogrow_customer/main/main.dart';
+import 'package:go_router/go_router.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  static const String routePath = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+        listener: (context, state) {
+          if (state is AppStarted) {
+            context.loaderOverlay.show();
+          } else if (state is Authenticated) {
+            context.loaderOverlay.hide();
+            GoRouter.of(context).go(MainPage.routePath);
+          } else if (state is UnAuthenticated) {
+            context.loaderOverlay.hide();
+            GoRouter.of(context).go(MainPage.routePath);
+          }
+        },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
